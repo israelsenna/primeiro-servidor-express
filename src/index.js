ffb23bddcff6a3d4cfd6c30edd4429c5eb46084e
Israel Senna <israel.senna@hotmail.com>
2023-08-13 18:34:28 -0300

    Subindo pasta primeiro-servidor-express

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/home', (requisicao, resposta) => {
+    resposta.send('Olá... Esse é o meu primeiro servidor com o express');
+});
+
+app.get('/array', (requisicao, resposta) => {
+    const array = [1, 2, 3, 4, 5];
+    resposta.send(array);
+});
+
+app.listen(3000);
